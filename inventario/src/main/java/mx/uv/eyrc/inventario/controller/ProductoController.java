b0b8c3286a2217f85a465162df01fc010354808d
RODRIGUEZ CASTELLANOS ERICK YAMIL <castellanosyamil@gmail.com>
2026-04-17 12:47:15 -0600

    Corregido

--- inventario/src/main/java/mx/uv/eyrc/inventario/controller/ProductoController.java
+++ inventario/src/main/java/mx/uv/eyrc/inventario/controller/ProductoController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
 import mx.uv.eyrc.inventario.entity.Producto;
-import mx.uv.eyrc.inventario.repository.ProductoService;
+import mx.uv.eyrc.inventario.service.ProductoService;
 
 @RestController
 @RequestMapping("/api/productos")
